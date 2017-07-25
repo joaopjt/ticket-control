276dbba4db6b3dd95ac7ef10dd7a452a2f52e413
João Julio <joao.tavares@novahaus.com.br>
2017-07-25 14:07:56 -0300

    Added migration to create users table

---
+++ server/migrations/20170725134351_create_users_table.js
@@ -0,0 +1,21 @@
+
+exports.up = function(knex, Promise) {
+  knex.schema.createTableIfNotExists('users', function(table) {
+    table.increments();
+    table.string('name');
+    table.string('email');
+    table.string('pass');
+    table.string('doc-number');
+    table.timestamps();
+  }).then(function() {
+    console.log('Table "users" created with success!');
+  }).catch(function(err) {
+    console.log('Error at create "users" table: ', err);
+  });  
+};
+
+exports.down = function(knex, Promise) {
+  knex.schema.dropTableIfExists('users', function() {
+    console.log('Table "users" dropped with success!');
+  })
+};
