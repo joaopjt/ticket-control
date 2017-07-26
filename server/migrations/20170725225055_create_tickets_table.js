e558e8b4fe61d4be65b8393cde56cc341e8f2429
João Julio <joaopjt@gmail.com>
2017-07-25 23:11:55 -0300

    Added migration to tickets table

---
+++ server/migrations/20170725225055_create_tickets_table.js
@@ -0,0 +1,21 @@
+
+exports.up = function(knex, Promise) {
+  knex.schema.createTableIfNotExists('tickets', function(table) {
+    table.increments();
+    table.integer('user_id').unsigned();
+    table.foreign('user_id').references('users.id');
+    table.string('event');
+    table.string('ticket_hash');
+    table.string('avaiable');
+  }).then(function() {
+    console.log('Table "tickets" created with success!');
+  }).catch(function(err) {
+    console.log('Error at create "tickets" table: ', err);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  knex.schema.dropTableIfExists('tickets', function() {
+    console.log('Table "tickets" dropped with success!');
+  });
+};
